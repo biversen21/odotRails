afa19cbb627c6ddf3d6be641db22ebaaffbbacdf
Brian Iversen <biversen21@gmail.com>
2014-06-29 19:18:39 -0700

    added edit item spec and code

--- spec/features/todo_items/edit_spec.rb
+++ spec/features/todo_items/edit_spec.rb
@@ -13,13 +13,39 @@ describe "Editing todo items" do
   
   it "is successful with valid content" do
     visit_todo_list(todo_list)
-    within("#todo_items_#{todo_item.id}") do
+    within("#todo_item_#{todo_item.id}") do
       click_link "Edit"
     end
     fill_in "Content", with: "Lots of Milk"
     click_button "Save"
     expect(page).to have_content("Saved todo list item.")
     todo_item.reload
-    expect(todo_item.title).to eq("Lots of Milk")
+    expect(todo_item.content).to eq("Lots of Milk")
+  end
+  
+  it "is unsuccessful with no content" do
+    visit_todo_list(todo_list)
+    within("#todo_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: ""
+    click_button "Save"
+    expect(page).to_not have_content("Saved todo list item.")
+    expect(page).to have_content("Content can't be blank")
+    todo_item.reload
+    expect(todo_item.content).to eq("Milk")
+  end
+  
+  it "is unsuccessful with not enough content" do
+    visit_todo_list(todo_list)
+    within("#todo_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: "1"
+    click_button "Save"
+    expect(page).to_not have_content("Saved todo list item.")
+    expect(page).to have_content("Content is too short")
+    todo_item.reload
+    expect(todo_item.content).to eq("Milk")
   end
 end
